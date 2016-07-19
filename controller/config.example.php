7e94ee06b338669337b0bba9c8e7e0094bc470fa
Bjørn Gustav Baklid <bg@dyret.net>
2016-07-19 21:36:38 +0200

    Added colors for 25 and 75 percent

--- controller/config.example.php
+++ controller/config.example.php
@@ -11,5 +11,7 @@ $data['localhost']['eth0']['dstLEDcount'] = 50;
 // The number is the percentage usage for the color
 $color_nolink = "FF0000";
 $colors[0] = '0000FF';
+$colors[25] = '0070FF'
 $colors[50] = '00FFFF';
+$colors[75] = '00FF40'
 $colors[95] = '00FF00';
